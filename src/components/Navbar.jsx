80e6a2ce4426e0814cecdaa486513d7aa18306d3
Celal <binaycelalselim@gmail.com>
2025-03-06 15:14:38 +0300

    navbar and route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import {
+  Disclosure,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/react";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <Disclosure as="nav" className="bg-[#080705]">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <Link className="pr-2 text-2xl font-semibold text-[#C3AC44]" to="/">
+            DVS Cinema
+          </Link>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <h5 className="mr-2 capitalize text-[#5A0A09] font-bold">
+              Dzelal Velislav Sokolovic
+            </h5>
+            {/* Profile dropdown */}
+            <Menu as="div" className="relative ml-3">
+              <div>
+                <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">Open user menu</span>
+                  <img
+                    alt=""
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    className="size-8 rounded-full"
+                  />
+                </MenuButton>
+              </div>
+              <MenuItems
+                transition
+                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+              >
+                <MenuItem>
+                  <Link
+                    to="/register"
+                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                  >
+                    Register
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link
+                    to="/login"
+                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                  >
+                    Login
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <span
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden cursor-pointer"
+                  >
+                    Log out
+                  </span>
+                </MenuItem>
+              </MenuItems>
+            </Menu>
+          </div>
+        </div>
+      </div>
+    </Disclosure>
+  );
 }
-
-export default Navbar
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
-  )
-}
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
